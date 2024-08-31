42b915c4aebf9b0cdc4f8fc705e9145ad25c1d92
Abhay Upadhyay <urabhay10@gmail.com>
2024-08-31 14:25:59 +0530

    algorithm update for low amount of cars

--- model2/Simulation/Traffic-Simulation/configCustom.py
+++ model2/Simulation/Traffic-Simulation/configCustom.py
@@ -1,6 +1,6 @@
 class config():
   def __init__(self):
-    self.pRoad = [1, 0.00, 0.00, 0.00]
+    self.pRoad = [0.2, 0.3, 0.2, 0.3]
     self.pLane = [0.25, 0.50, 0.25]
 
     self.vehicles = [0.7, 0.1, 0.1, 0.1]
